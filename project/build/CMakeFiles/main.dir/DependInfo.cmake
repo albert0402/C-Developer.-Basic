e986e9870de7929cb5ec7f41965f05663fec6ccc
albertbolotnov <bolotnovalbert@mail.ru>
2024-09-21 17:02:51 +0300

    it works

---
+++ project/build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/albertbolotnov/Desktop/otus/otus_C-Developer.Basic/project/calculations/AstroInertialModeResultMatrix.cpp" "CMakeFiles/main.dir/calculations/AstroInertialModeResultMatrix.cpp.o" "gcc" "CMakeFiles/main.dir/calculations/AstroInertialModeResultMatrix.cpp.o.d"
+  "/Users/albertbolotnov/Desktop/otus/otus_C-Developer.Basic/project/calculations/CoorectionModeResultMatrix.cpp" "CMakeFiles/main.dir/calculations/CoorectionModeResultMatrix.cpp.o" "gcc" "CMakeFiles/main.dir/calculations/CoorectionModeResultMatrix.cpp.o.d"
+  "/Users/albertbolotnov/Desktop/otus/otus_C-Developer.Basic/project/calculations/Result.cpp" "CMakeFiles/main.dir/calculations/Result.cpp.o" "gcc" "CMakeFiles/main.dir/calculations/Result.cpp.o.d"
+  "/Users/albertbolotnov/Desktop/otus/otus_C-Developer.Basic/project/main.cpp" "CMakeFiles/main.dir/main.cpp.o" "gcc" "CMakeFiles/main.dir/main.cpp.o.d"
+  "/Users/albertbolotnov/Desktop/otus/otus_C-Developer.Basic/project/src/AstroMatrix.cpp" "CMakeFiles/main.dir/src/AstroMatrix.cpp.o" "gcc" "CMakeFiles/main.dir/src/AstroMatrix.cpp.o.d"
+  "/Users/albertbolotnov/Desktop/otus/otus_C-Developer.Basic/project/src/ConjugateMatrix.cpp" "CMakeFiles/main.dir/src/ConjugateMatrix.cpp.o" "gcc" "CMakeFiles/main.dir/src/ConjugateMatrix.cpp.o.d"
+  "/Users/albertbolotnov/Desktop/otus/otus_C-Developer.Basic/project/src/GeographyMatrix.cpp" "CMakeFiles/main.dir/src/GeographyMatrix.cpp.o" "gcc" "CMakeFiles/main.dir/src/GeographyMatrix.cpp.o.d"
+  "/Users/albertbolotnov/Desktop/otus/otus_C-Developer.Basic/project/src/Matrix.cpp" "CMakeFiles/main.dir/src/Matrix.cpp.o" "gcc" "CMakeFiles/main.dir/src/Matrix.cpp.o.d"
+  "/Users/albertbolotnov/Desktop/otus/otus_C-Developer.Basic/project/src/Rotation.cpp" "CMakeFiles/main.dir/src/Rotation.cpp.o" "gcc" "CMakeFiles/main.dir/src/Rotation.cpp.o.d"
+  "/Users/albertbolotnov/Desktop/otus/otus_C-Developer.Basic/project/src/TimeMatrix.cpp" "CMakeFiles/main.dir/src/TimeMatrix.cpp.o" "gcc" "CMakeFiles/main.dir/src/TimeMatrix.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ project/build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/calculations/AstroInertialModeResultMatrix.cpp.o"
+  "CMakeFiles/main.dir/calculations/AstroInertialModeResultMatrix.cpp.o.d"
+  "CMakeFiles/main.dir/calculations/CoorectionModeResultMatrix.cpp.o"
+  "CMakeFiles/main.dir/calculations/CoorectionModeResultMatrix.cpp.o.d"
+  "CMakeFiles/main.dir/calculations/Result.cpp.o"
+  "CMakeFiles/main.dir/calculations/Result.cpp.o.d"
+  "CMakeFiles/main.dir/main.cpp.o"
+  "CMakeFiles/main.dir/main.cpp.o.d"
+  "CMakeFiles/main.dir/src/AstroMatrix.cpp.o"
+  "CMakeFiles/main.dir/src/AstroMatrix.cpp.o.d"
+  "CMakeFiles/main.dir/src/ConjugateMatrix.cpp.o"
+  "CMakeFiles/main.dir/src/ConjugateMatrix.cpp.o.d"
+  "CMakeFiles/main.dir/src/GeographyMatrix.cpp.o"
+  "CMakeFiles/main.dir/src/GeographyMatrix.cpp.o.d"
+  "CMakeFiles/main.dir/src/Matrix.cpp.o"
+  "CMakeFiles/main.dir/src/Matrix.cpp.o.d"
+  "CMakeFiles/main.dir/src/Rotation.cpp.o"
+  "CMakeFiles/main.dir/src/Rotation.cpp.o.d"
+  "CMakeFiles/main.dir/src/TimeMatrix.cpp.o"
+  "CMakeFiles/main.dir/src/TimeMatrix.cpp.o.d"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
